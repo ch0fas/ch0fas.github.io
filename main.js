05f819fb149e2fadf06fb86cbf78a619f561bd63
ch0fas <sof.mal.gar55@gmail.com>
2025-09-02 09:39:24 -0600

    Added Humbug to MREG, also created Writing Page and linked to it in main page, changed logic of date indicator in JS

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ year = fecha.getFullYear();
 mes = fecha.getMonth() + 1;
 dia = fecha.getDate();
 
-if (mes < 10) {
+if (mes < 10 && dia < 10) {
+    document.getElementById("eldia").innerHTML = "0" + dia + "/0" + mes + "/" + year
+} else if (mes > 10 && dia < 10)  {
+    document.getElementById("eldia").innerHTML = "0" + dia + "/" + mes + "/" + year
+} else if (mes < 10 && dia > 10) {
     document.getElementById("eldia").innerHTML = dia + "/0" + mes + "/" + year
 } else {
     document.getElementById("eldia").innerHTML = dia + "/" + mes + "/" + year
 }
-
 // Light-Dark Mode 
