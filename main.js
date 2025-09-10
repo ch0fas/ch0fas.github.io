ad5adb6db52300b4b2d98902c37c74042b0a1473
ch0fas <sof.mal.gar55@gmail.com>
2025-09-10 12:48:34 -0600

    FInished first draft of the article template

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if (mes < 10 && dia < 10) {
     document.getElementById("eldia").innerHTML = "0" + dia + "/0" + mes + "/" + year
 } else if (mes > 10 && dia < 10)  {
     document.getElementById("eldia").innerHTML = "0" + dia + "/" + mes + "/" + year
-} else if (mes < 10 && dia > 10) {
+} else if (mes < 10 && dia >= 10) {
     document.getElementById("eldia").innerHTML = dia + "/0" + mes + "/" + year
 } else {
     document.getElementById("eldia").innerHTML = dia + "/" + mes + "/" + year
